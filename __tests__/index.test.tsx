86ffc8a46032e39b05af068d05960d19451d3a87
Iago Brayham <iagobrayhamcunha@hotmail.com>
2022-01-13 16:03:33 -0300

    Add redux provider for Home page test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import Home from "../pages/index";
+import { render } from "../utils/test-utils";
 
 describe("Home", () => {
   it("renders a heading", () => {
