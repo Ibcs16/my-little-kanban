88ed5725bd4e361236727b6d185433ee97fbd9b7
Iago Brayham <iagobrayhamcunha@hotmail.com>
2022-01-19 18:35:57 -0300

    show loading spinner when fetching data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
-import { useAppDispatch } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { Board } from "../components";
+import BoardDescription from "../components/BoardDescription";
+import BoardTitle from "../components/BoardTitle";
+import Button from "../components/Button";
+import Spinner from "../components/Spinner";
+import Typography from "../components/Typography";
 import SearchBox from "../features/todos/Searchbox";
 import StatusFilter from "../features/todos/StatusFilter";
-import { fetchTodos } from "../features/todos/todosSlice";
+import { fetchTodos, selectApiStatus } from "../features/todos/todosSlice";
 
 const Home: NextPage = () => {
   const dispatch = useAppDispatch();
+  const apiStatus = useAppSelector(selectApiStatus);
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
@@ -20,11 +26,26 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <SearchBox />
-        <StatusFilter />
-        <Board />
-      </main>
+      {apiStatus === "loading" && <Spinner />}
+      {apiStatus === "idle" && (
+        <main>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+              <BoardTitle>Tasks</BoardTitle>
+              <BoardDescription>
+                This is your personal kanban, to help you organize and aways be
+                aware of your activities 😉
+              </BoardDescription>
+            </div>
+            <div>
+              <SearchBox />
+              <StatusFilter />
+            </div>
+            <Button label="Add task" icon="plus" />
+          </div>
+          <Board />
+        </main>
+      )}
     </div>
   );
 };
